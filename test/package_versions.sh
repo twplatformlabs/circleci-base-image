7bb312cc2fdb459eed885bc6d212d518c949cd73
Nic Cheneweth <nchenewe@thoughtworks.com>
2026-02-04 22:30:36 -0600

    [nc] test release notes
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

--- test/package_versions.sh
+++ test/package_versions.sh
@@ -7,6 +7,8 @@ fi
 
 bash -c "docker exec ${TEST_CONTAINER} gh --version" && echo
 bash -c "docker exec ${TEST_CONTAINER} docker --version" && echo
+OP_RESULT=$(bash -c "docker exec ${TEST_CONTAINER} op --version")
+echo "1Password version ${OP_RESULT}"
 bash -c "docker exec ${TEST_CONTAINER} op --version" && echo
 bash -c "docker exec ${TEST_CONTAINER} vault --version" && echo
 bash -c "docker exec ${TEST_CONTAINER} teller version" && echo
